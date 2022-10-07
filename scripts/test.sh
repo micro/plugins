13b2e3d9ba1dd193a8003c39f0c6c734a0cc5cda
Davincible <david.brouwer.99@gmail.com>
2022-10-07 22:51:41 +0200

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -101,10 +101,8 @@ case $1 in
 "lint")
 	dirs=$(get_dirs $2)
 
-	msg="Found $(echo $dirs | wc -l) changed directories"
-        printf '\033[46m' ${msg} '\033{m\n'
-	msg="Changed dirs:"
-        printf '\033[46m' ${msg} '\033{m\n'
+        printf '\033[46m' "Found $(echo $dirs | wc -l) changed directories" '\033{m\n'
+        printf '\033[46m' "Changed dirs:" '\033{m\n'
 	printf "${dirs}\n"
 	sleep 1
 
