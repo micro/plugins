18a3e6f4702ee0926bb3d087b38fb153133b0fe5
Davincible <david.brouwer.99@gmail.com>
2022-10-07 21:07:29 +0200

    feat: upgrade CI pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,27 +1,93 @@
 #!/bin/bash -e
 
-version=$1
+GO_TEST_FLAGS="-v -race -cover -bench=."
 
-if [ "x$version" = "x" ]; then
-  echo "must specify version"
-  exit 1;
-fi
+# Find directories that contain changes
+function find_changes() {
+	# Pull main branch
+	git checkout main &>/dev/null
+	git checkout $GITHUB_REF_NAME &>/dev/null
 
-for d in $(find $version -name 'go.mod'); do
-  case $(dirname $d) in
-    'v2/agent/command/animate'|'v2/broker/googlepubsub'|'v2/config/source/configmap'|'v2/config/source/vault'|'v2/events/redis'|'v2/logger/windowseventlog'|'v2/logger/zerolog'|'v2/registry/kubernetes'|'v2/store/mysql'|'v2/store/redis'|'v2/store/memory'|'v2/store/file'|'v2/sync/consul'|'v2/sync/etcd')
-    echo SKIP $(dirname $d)
-    ;;
-    'v3/config/source/configmap'|'v3/config/source/vault'|'v3/events/redis'|'v3/logger/apex'|'v3/logger/windowseventlog'|'v3/store/mysql'|'v3/store/redis'|'v3/store/memory'|'v3/store/file'|'v3/sync/consul'|'v3/sync/etcd')
-    echo SKIP $(dirname $d)
-    ;;
-    'v4/config/source/configmap'|'v4/config/source/vault'|'v4/events/redis'|'v4/events/natsjs'|'v4/logger/windowseventlog'|'v4/store/mysql'|'v4/store/redis'|'v4/sync/consul'|'v4/sync/etcd'|'v4/cache/redis')
-    echo SKIP $(dirname $d)
-    ;;
-    *)
-    pushd $(dirname $d) >/dev/null
-    go vet
-    go test 
-    popd >/dev/null
-  esac
-done
+	# Find all directories that have changed files
+	hash=$(git merge-base --fork-point main)
+	changes=$(git diff --name-only $hash |
+		xargs -d'\n' -I{} dirname {} | sort -u)
+
+	changes=$(find $(echo $changes) -name 'go.mod' -printf '%h\n')
+
+	echo $changes
+}
+
+# Find all go directories
+function find_all() {
+	find . -name 'go.mod' -printf '%h\n'
+}
+
+# Run GoLangCi Linters
+function run_linter() {
+	cwd=$(pwd)
+	for dir in $(echo $1); do
+		echo "Running linter on $dir"
+		pushd $dir >/dev/null
+		golangci-lint run -c "${cwd}/.golangci.yaml"
+		popd >/dev/null
+	done
+}
+
+# Run unit tests with tparse to create a summary
+function create_summary() {
+	go install github.com/mfridman/tparse@latest
+
+	# Download all modules
+	go get -v -t -d ./...
+
+	go test $GO_TEST_FLAGS -json ./... |
+		tparse -notests -format=markdown >>$GITHUB_STEP_SUMMARY
+}
+
+# Run Unit tests with RichGo for pretty output
+function run_test() {
+	go install github.com/kyoh86/richgo@latest
+
+	# Download all modules
+	go get -v -t -d ./...
+
+	richgo test $GO_TEST_FLAGS ./...
+}
+
+# Get the dir list based on command type
+function get_dirs() {
+	if [[ $1 == "all" ]]; then
+		find_all
+	else
+		find_changes
+	fi
+}
+
+case $1 in
+"lint")
+	dirs=$(get_dirs $2)
+
+	echo "Found $(echo $changed | wc -l) changed directories"
+	echo "Changed dirs:"
+	echo $dirs
+
+	run_linter $dirs
+	;;
+"test")
+	dirs=$(get_dirs $2)
+	run_test $dirs
+	;;
+"summary")
+	dirs=$(get_dirs $2)
+	create_summary $dirs
+	;;
+"")
+	echo "Please provider a command"
+	exit 1
+	;;
+*)
+	echo "Command not found: $1"
+	exit 1
+	;;
+esac
