65ba41c353173f909a2d3cd44a6b080de15363c2
Davincible <david.brouwer.99@gmail.com>
2022-10-08 14:38:56 +0200

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -73,7 +73,7 @@ function get_dirs() {
 function run_linter() {
   curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 
-  print_msg $(golangci-lint --version)
+  print_msg "$(golangci-lint --version)"
 
   cwd=$(pwd)
   dirs=$1
