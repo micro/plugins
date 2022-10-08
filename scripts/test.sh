f5e03eb6a85bb916482cf5d63c27929de6804954
Davincible <david.brouwer.99@gmail.com>
2022-10-08 03:20:33 +0200

    fix(CI): bar length

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ GO_TEST_FLAGS="-v -race -cover -bench=."
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
-BAR="------------------------------------------------"
+BAR="-------------------------------------------------------------------------------"
 
 export RICHGO_FORCE_COLOR="true"
 export IN_TRAVIS_CI="true"
