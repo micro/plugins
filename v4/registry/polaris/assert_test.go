b4c7da25bb6d728e7461b4a14a94d7fdf49d8a8f
Davincible <david.brouwer.99@gmail.com>
2022-10-20 15:57:10 +0200

    fix: more lintint issues

--- v4/registry/polaris/assert_test.go
+++ v4/registry/polaris/assert_test.go
@@ -7,18 +7,21 @@ import (
 	"go-micro.dev/v4/registry"
 )
 
+//nolint:thelper
 func assertNoError(tb testing.TB, actual error) {
 	if actual != nil {
 		tb.Errorf("expected no error, got %v", actual)
 	}
 }
 
+//nolint:thelper
 func assertEqual(tb testing.TB, expected, actual interface{}) {
 	if !reflect.DeepEqual(expected, actual) {
 		tb.Errorf("expected %v, got %v", expected, actual)
 	}
 }
 
+//nolint:thelper
 func assertSrvLen(tb testing.TB, expected int, srvs []*registry.Service) {
 	if len(srvs) != expected {
 		tb.Log("About to error:")
--- v4/registry/polaris/options.go
+++ v4/registry/polaris/options.go
@@ -19,42 +19,48 @@ type authCreds struct {
 	Password string
 }
 
-// Auth allows you to specify username/password
-// just no effect
+// Auth allows you to specify username/password.
+// just no effect.
 func Auth(username, password string) registry.Option {
 	return func(o *registry.Options) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
+
 		o.Context = context.WithValue(o.Context, authKey{}, &authCreds{Username: username, Password: password})
 	}
 }
 
+// NameSpace sets the namespace.
 func NameSpace(namespace string) registry.Option {
 	return func(o *registry.Options) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
+
 		o.Context = context.WithValue(o.Context, nameSpaceKey{}, namespace)
 	}
 }
 
+// ServerToken sets the server token.
 func ServerToken(token string) registry.Option {
 	return func(o *registry.Options) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
+
 		o.Context = context.WithValue(o.Context, serverTokenKey{}, token)
 	}
 }
 
-// only get one instance for use polaris's loadbalance
-// and diasble cache by: github.com/go-micro/plugins/v4/selector/registry, option TTF(0)
+// GetOneInstance will fetch only one instance for use with Polaris's loadbalancer
+// and disable cache by: github.com/go-micro/plugins/v4/selector/registry, option TTF(0).
 func GetOneInstance(flag bool) registry.Option {
 	return func(o *registry.Options) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
+
 		o.Context = context.WithValue(o.Context, getOneInstanceKey{}, flag)
 	}
 }
